3ff92c970ff08ed3afc61ea08fb87f53662ae961
mslearing <maria@systembase.co.jp>
2018-03-22 19:03:32 +0900

    require関数

--- 20180315/multi_event.js
+++ 20180315/multi_event.js
@@ -2,18 +2,18 @@ var http = require('http');
 
 var server = http.createServer();
 
-server.on('request',function(request,response) {
+server.on('request',(request,response) => {
 	console.log('request received'); 
 	    });
 
-server.on('request',function(request,response) {
+server.on('request',(request,response)=> {
 	console.log(request.url);
 	response.writeHead(200,{'Content-Type':'text/plain'}
 	      );
 	response.end('hello' + request.url);
 	     });
 
-server.on('request',function(request,response) {
+server.once('request',(request,response)=> {
 	console.log('response sent.');
 	    });
 	    
---
+++ 20180322/hello.js
@@ -0,0 +1,10 @@
+var myName = 'Taro'
+exports.myName = myName;
+
+function greeting(something) {
+	console.log(myName + ' hello, ' + something + '!');
+};
+
+exports.greeting = greeting;
+
+greeting('world!');
