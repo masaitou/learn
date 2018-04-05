704af4d25cab38d8fe579dac5d0f8a2ea1d18774
mslearing <maria@systembase.co.jp>
2018-04-05 19:03:51 +0900

    Webアプリケーションについて

---
+++ WebApp/WebSample.js
@@ -0,0 +1,20 @@
+var http = require('http');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+	response.writeHead(200);
+	
+	response.write('URL: ' + request.url + '\n');
+	
+	response.write('Method: ' + request.method + '\n');
+	
+	Object.keys(request.headers).forEach(function (key) {
+		response.write(key + ': ' +request.headers[key] + '\n');
+	});
+	
+	response.end();
+
+});
+
+server.listen(80, '127.0.0.1')
